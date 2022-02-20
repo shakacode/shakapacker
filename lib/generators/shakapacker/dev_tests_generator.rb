a90d0818511564d05532148244f9ea9a93238be9
Saiqul Haq <saiqulhaq@gmail.com>
2022-02-20 09:19:45 +0700

    remove unused methods

--- lib/generators/shakapacker/dev_tests_generator.rb
+++ lib/generators/shakapacker/dev_tests_generator.rb
@@ -9,47 +9,6 @@ module Shakapacker
       include GeneratorHelper
       Rails::Generators.hide_namespace(namespace)
       source_root(File.expand_path("templates/dev_tests", __dir__))
-
-      def copy_rspec_files
-        %w[.eslintrc
-           spec/spec_helper.rb
-           spec/rails_helper.rb
-           spec/simplecov_helper.rb
-           .rspec].each { |file| copy_file(file) }
-      end
-
-      def copy_tests
-        %w[spec/system/hello_world_spec.rb].each { |file| copy_file(file) }
-      end
-
-      def add_test_related_gems_to_gemfile
-        gem("rspec-rails", group: :test)
-        gem("chromedriver-helper", group: :test)
-        gem("coveralls", require: false)
-      end
-
-      def replace_prerender_if_server_rendering
-        return unless options.example_server_rendering
-
-        hello_world_index = File.join(destination_root, "app", "views", "hello_world", "index.html.erb")
-        hello_world_contents = File.read(hello_world_index)
-        new_hello_world_contents = hello_world_contents.gsub(/prerender: false/,
-                                                             "prerender: true")
-
-        File.open(hello_world_index, "w+") { |f| f.puts new_hello_world_contents }
-      end
-
-      def add_yarn_relative_install_script_in_package_json
-        package_json = File.join(destination_root, "package.json")
-        contents = File.read(package_json)
-        replacement_value = <<-STRING
-  "scripts": {
-    "postinstall": "yalc link react-on-rails",
-        STRING
-        new_client_package_json_contents = contents.gsub(/ {2}"scripts": {/,
-                                                         replacement_value)
-        File.open(package_json, "w+") { |f| f.puts new_client_package_json_contents }
-      end
     end
   end
 end
