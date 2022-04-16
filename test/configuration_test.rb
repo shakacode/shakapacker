902968182c7768624613424c0e48b5cfa3ec70ab
Justin Gordon <justin@shakacode.com>
2022-04-15 23:04:03 -1000

    Put back config.public_manifest_path

--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -38,6 +38,11 @@ class ConfigurationTest < Webpacker::Test
     assert_equal @config.public_output_path.to_s, public_output_path
   end
 
+  def test_public_manifest_path
+    public_manifest_path = File.expand_path File.join(File.dirname(__FILE__), "test_app/public/packs", "manifest.json").to_s
+    assert_equal @config.public_manifest_path.to_s, public_manifest_path
+  end
+
   def test_manifest_path
     manifest_path = File.expand_path File.join(File.dirname(__FILE__), "test_app/public/packs", "manifest.json").to_s
     assert_equal @config.manifest_path.to_s, manifest_path
