29ebb2f2017424f6b55da54554b8ace533a5f7c2
Tom Dracz <tom.dracz@sanctus.io>
2022-04-20 10:24:22 +0100

    Update tests

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -9,33 +9,48 @@ class CompilerTest < Minitest::Test
     Webpacker.compiler.env = {}
   end
 
-  def test_freshness
-    assert Webpacker.compiler.stale?
-    assert !Webpacker.compiler.fresh?
+  def setup
+    @manifest_timestamp = Time.parse("2021-01-01 12:34:56 UTC")
+  end
+
+  def with_stubs(latest_timestamp:, manifest_exists: true, &proc)
+    @latest_timestamp = latest_timestamp
+
+    Webpacker.compiler.stub :latest_modified_timestamp, @latest_timestamp do
+      FileTest.stub :exist?, manifest_exists do
+        File.stub :mtime, @manifest_timestamp do
+          yield proc
+        end
+      end
+    end
+  end
+
+  def test_freshness_when_manifest_missing
+    latest_timestamp = @manifest_timestamp + 3600
+
+    with_stubs(latest_timestamp: latest_timestamp.to_i, manifest_exists: false) do
+      assert Webpacker.compiler.stale?
+    end
+  end
+
+  def test_freshness_when_manifest_older
+    latest_timestamp = @manifest_timestamp + 3600
+
+    with_stubs(latest_timestamp: latest_timestamp.to_i) do
+      assert Webpacker.compiler.stale?
+    end
+  end
+
+  def test_freshness_when_manifest_newer
+    latest_timestamp = @manifest_timestamp - 3600
+
+    with_stubs(latest_timestamp: latest_timestamp.to_i) do
+      assert Webpacker.compiler.fresh?
+    end
   end
 
   def test_compile
     assert !Webpacker.compiler.compile
-  end
-
-  def test_freshness_on_compile_success
-    status = OpenStruct.new(success?: true)
-
-    assert Webpacker.compiler.stale?
-    Open3.stub :capture3, [:sterr, :stdout, status] do
-      Webpacker.compiler.compile
-      assert Webpacker.compiler.fresh?
-    end
-  end
-
-  def test_freshness_on_compile_fail
-    status = OpenStruct.new(success?: false)
-
-    assert Webpacker.compiler.stale?
-    Open3.stub :capture3, [:sterr, :stdout, status] do
-      Webpacker.compiler.compile
-      assert Webpacker.compiler.fresh?
-    end
   end
 
   def test_external_env_variables
