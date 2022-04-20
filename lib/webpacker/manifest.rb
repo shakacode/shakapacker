7c4956e0c6b17094b7de7dce1fc4c2f84fc00f29
Justin Gordon <justin@shakacode.com>
2022-04-19 21:32:55 -1000

    Update manifest.rb

--- lib/webpacker/manifest.rb
+++ lib/webpacker/manifest.rb
@@ -104,12 +104,12 @@ class Webpacker::Manifest
 
     def missing_file_from_manifest_error(bundle_name)
       <<-MSG
-Webpacker can't find #{bundle_name} in #{config.manifest_path}. Possible causes:
+Shakapacker can't find #{bundle_name} in #{config.manifest_path}. Possible causes:
 1. You forgot to install node packages (try `yarn install`) or are running an incompatible version of Node
 2. Your app has code with a non-standard extension (like a `.jsx` file) but the extension is not in the `extensions` config in `config/webpacker.yml`
 3. You have set compile: false (see `config/webpacker.yml`) for this environment
    (unless you are using the `bin/webpacker -w` or the `bin/webpacker-dev-server`, in which case maybe you aren't running the dev server in the background?)
-4. webpack has not yet re-run to reflect updates.
+4. webpack has not yet FINISHED running to reflect updates.
 5. You have misconfigured Webpacker's `config/webpacker.yml` file.
 6. Your webpack configuration is not creating a manifest.
 
