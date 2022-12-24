9e14f12fc01411531d9b690668f2a7fac9e27e8b
Mostafa Ahangarha <ahangarha@riseup.net>
2022-12-24 17:44:46 +0330

    Rewrite Webpacker::MtimeStrategy specs

--- spec/mtime_strategy_spec.rb
+++ spec/mtime_strategy_spec.rb
@@ -1,42 +1,37 @@
-require "test_helper"
-
-class MtimeStrategyTest < Minitest::Test
-  def setup
+describe "Webpacker::MtimeStrategy" do
+  before :all do
     @mtime_strategy = Webpacker::MtimeStrategy.new
     @manifest_timestamp = Time.parse("2021-01-01 12:34:56 UTC")
   end
 
   def with_stubs(latest_timestamp:, manifest_exists: true)
-    @mtime_strategy.stub :latest_modified_timestamp, latest_timestamp do
-      FileTest.stub :exist?, manifest_exists do
-        File.stub :mtime, @manifest_timestamp do
-          yield
-        end
-      end
-    end
+    allow(@mtime_strategy).to receive(:latest_modified_timestamp).and_return(latest_timestamp)
+    allow(FileTest).to receive(:exist?).and_return(manifest_exists)
+    allow(File).to receive(:mtime).and_return(@manifest_timestamp)
+    yield
   end
 
-  def test_freshness_when_manifest_missing
+  it "#stale? returns true when manifest is missing" do
     latest_timestamp = @manifest_timestamp + 3600
 
     with_stubs(latest_timestamp: latest_timestamp.to_i, manifest_exists: false) do
-      assert @mtime_strategy.stale?
+      expect(@mtime_strategy.stale?).to be true
     end
   end
 
-  def test_freshness_when_manifest_older
+  it "#stale? returns true when manifest is older" do
     latest_timestamp = @manifest_timestamp + 3600
 
     with_stubs(latest_timestamp: latest_timestamp.to_i) do
-      assert @mtime_strategy.stale?
+      expect(@mtime_strategy.stale?).to be true
     end
   end
 
-  def test_freshness_when_manifest_newer
+  it "#fresh? returns true when manifest is new" do
     latest_timestamp = @manifest_timestamp - 3600
 
     with_stubs(latest_timestamp: latest_timestamp.to_i) do
-      assert @mtime_strategy.fresh?
+      expect(@mtime_strategy.fresh?).to be true
     end
   end
 end
