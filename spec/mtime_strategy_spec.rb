b9ea7d716ff4ec860d545c636ecb09837f19bde2
Mostafa Ahangarha <ahangarha@riseup.net>
2023-01-09 15:05:58 +0330

    Improve code organization
    Improve tests

--- spec/mtime_strategy_spec.rb
+++ spec/mtime_strategy_spec.rb
@@ -1,37 +1,53 @@
 describe "Webpacker::MtimeStrategy" do
-  before :all do
-    @mtime_strategy = Webpacker::MtimeStrategy.new
-    @manifest_timestamp = Time.parse("2021-01-01 12:34:56 UTC")
-  end
+  let(:mtime_strategy) { Webpacker::MtimeStrategy.new }
+  let(:manifest_timestamp) { Time.parse("2021-01-01 12:34:56 UTC") }
 
-  def with_stubs(latest_timestamp:, manifest_exists: true)
-    allow(@mtime_strategy).to receive(:latest_modified_timestamp).and_return(latest_timestamp)
-    allow(FileTest).to receive(:exist?).and_return(manifest_exists)
-    allow(File).to receive(:mtime).and_return(@manifest_timestamp)
-    yield
-  end
+  describe "#fresh?" do
+    it "returns false when manifest is missing" do
+      latest_timestamp = manifest_timestamp + 3600
 
-  it "#stale? returns true when manifest is missing" do
-    latest_timestamp = @manifest_timestamp + 3600
+      with_stubs(latest_timestamp: latest_timestamp.to_i, manifest_exists: false) do
+        expect(mtime_strategy.fresh?).to be false
+      end
+    end
 
-    with_stubs(latest_timestamp: latest_timestamp.to_i, manifest_exists: false) do
-      expect(@mtime_strategy.stale?).to be true
+    it "returns false when manifest is older" do
+      latest_timestamp = manifest_timestamp + 3600
+
+      with_stubs(latest_timestamp: latest_timestamp.to_i) do
+        expect(mtime_strategy.fresh?).to be false
+      end
+    end
+
+    it "returns true when manifest is new" do
+      latest_timestamp = manifest_timestamp - 3600
+
+      with_stubs(latest_timestamp: latest_timestamp.to_i) do
+        expect(mtime_strategy.fresh?).to be true
+      end
     end
   end
 
-  it "#stale? returns true when manifest is older" do
-    latest_timestamp = @manifest_timestamp + 3600
+  describe "#stale?" do
+    it "returns false when #fresh? is true" do
+      expect(mtime_strategy).to receive(:fresh?).and_return(true)
 
-    with_stubs(latest_timestamp: latest_timestamp.to_i) do
-      expect(@mtime_strategy.stale?).to be true
+      expect(mtime_strategy.stale?).to be false
+    end
+
+    it "returns true when #fresh? is false" do
+      expect(mtime_strategy).to receive(:fresh?).and_return(false)
+
+      expect(mtime_strategy.stale?).to be true
     end
   end
 
-  it "#fresh? returns true when manifest is new" do
-    latest_timestamp = @manifest_timestamp - 3600
+  private
 
-    with_stubs(latest_timestamp: latest_timestamp.to_i) do
-      expect(@mtime_strategy.fresh?).to be true
+    def with_stubs(latest_timestamp:, manifest_exists: true)
+      allow(mtime_strategy).to receive(:latest_modified_timestamp).and_return(latest_timestamp)
+      allow(FileTest).to receive(:exist?).and_return(manifest_exists)
+      allow(File).to receive(:mtime).and_return(manifest_timestamp)
+      yield
     end
-  end
 end
