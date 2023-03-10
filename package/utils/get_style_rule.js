9fef497cc604afbeaec59989ffbddf6cd2d0e02f
Alexander Talyuk <sshnbd@gmail.com>
2023-03-10 14:04:49 -0800

    Set CSS modules mode depending on file type

--- package/utils/get_style_rule.js
+++ package/utils/get_style_rule.js
@@ -2,6 +2,8 @@
 const { canProcess, moduleExists } = require('./helpers')
 const inliningCss = require('../inliningCss')
 
+const isModuleFile = (filename) => !!filename.match(/\.module\.\w+(\.erb)?$/i)
+
 const getStyleRule = (test, preprocessors = []) => {
   if (moduleExists('css-loader')) {
     const tryPostcss = () =>
@@ -18,7 +20,10 @@ const getStyleRule = (test, preprocessors = []) => {
         loader: require.resolve('css-loader'),
         options: {
           sourceMap: true,
-          importLoaders: 2
+          importLoaders: 2,
+          modules: {
+            mode: resourcePath => isModuleFile(resourcePath) ? 'local' : 'icss'
+          }
         }
       },
       tryPostcss(),
