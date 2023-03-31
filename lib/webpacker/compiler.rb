1cd90da7a7eb0c1a776d18b4465c2261d71a3509
Tom Dracz <tom.dracz@simplybusiness.co.uk>
2023-03-31 14:59:02 +0100

    Satisfy Rubocop

--- lib/webpacker/compiler.rb
+++ lib/webpacker/compiler.rb
@@ -55,7 +55,7 @@ class Webpacker::Compiler
     end
 
     def open_lock_file
-      lock_file_name = config.root_path.join('tmp/shakapacker.lock')
+      lock_file_name = config.root_path.join("tmp/shakapacker.lock")
       File.open(lock_file_name, File::CREAT) do |lf|
         return yield lf
       end
