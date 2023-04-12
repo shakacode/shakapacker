740e6ea4903cb10552a073e67ebadabf469bc87c
Mostafa Ahangarha <ahangarha@riseup.net>
2023-04-12 15:22:31 +0330

    Move code related to devServer into dev_server.js
    The logic for mapping keys in the shakapacker.yml file is straightforward: if
    a key is specific to webpack, it will be mapped to camelCase, otherwise, the
    key will remain as is. This means that any other key that is not specific to
    webpack (such as random_key) will remain accessible in its original form.

--- package/environments/development.js
+++ package/environments/development.js
@@ -1,10 +1,8 @@
 const { merge } = require('webpack-merge')
 
 const baseConfig = require('./base')
-const devServer = require('../dev_server')
+const devServerConfig = require('../dev_server')
 const { runningWebpackDevServer } = require('../env')
-
-const { outputPath: contentBase, publicPath } = require('../config')
 
 let devConfig = {
   mode: 'development',
@@ -12,55 +10,6 @@ let devConfig = {
 }
 
 if (runningWebpackDevServer) {
-  const liveReload = devServer.live_reload !== undefined ? devServer.live_reload : !devServer.hmr
-
-  const devServerConfig = {
-    devMiddleware: {
-      publicPath
-    },
-    compress: devServer.compress,
-    allowedHosts: devServer.allowed_hosts,
-    host: devServer.host,
-    port: devServer.port,
-    hot: devServer.hmr,
-    liveReload,
-    historyApiFallback: { disableDotRule: true },
-    headers: devServer.headers,
-    static: {
-      publicPath: contentBase
-    }
-  }
-
-  if (devServer.static) {
-    devServerConfig.static = { ...devServerConfig.static, ...devServer.static }
-  }
-
-  if (devServer.client) {
-    devServerConfig.client = devServer.client
-  }
-
-  // If we have `server` entry, we ignore possible `https` one.
-  if (devServer.server) {
-    devServerConfig.server = devServer.server
-  } else if (devServer.https) {
-    devServerConfig.https = devServer.https
-  }
-
-  const jsToRbKeyPairs = [
-    ['magicHtml', 'magic_html'],
-    ['onAfterSetupMiddleware', 'on_after_setup_middleware'],
-    ['onBeforeSetupMiddleware', 'on_before_setup_middleware'],
-    ['onListening', 'on_listening'],
-    ['setupExitSignals', 'setup_exit_signals'],
-    ['setupMiddlewares', 'setup_middlewares'],
-    ['watchFiles', 'watch_files'],
-    ['webSocketServer', 'web_socket_server']
-  ]
-
-  jsToRbKeyPairs.forEach(([jsKey, rbKey]) => {
-    if (devServer[rbKey]) devServerConfig[jsKey] = devServer[rbKey]
-  })
-
   devConfig = merge(devConfig, {
     stats: {
       colors: true,
