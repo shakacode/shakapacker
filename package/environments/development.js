0982b95cab6f4b3f08ecc22ffb21dff9f8ff8394
Justin Gordon <justin@shakacode.com>
2023-05-06 16:40:46 -1000

    Revert changes which break HMR
    Due to extra keys added to webpack config.
    
    Error like:
    wp-client | [webpack-cli] Invalid options object. Dev Server has been initialized using an options object that does not match the API schema.
    wp-client |  - options has an unknown property 'pretty'. These properties are valid:
    wp-client |    object { allowedHosts?, bonjour?, client?, compress?, devMiddleware?, headers?, historyApiFallback?, host?, hot?, http2?, https?, ipc?, liveReload?, magicHtml?, onAfterSetupMiddleware?, onBeforeSetupMiddleware?, onListening?, open?, port?, proxy?, server?, setupExitSignals?, setupMiddlewares?, static?, watchFiles?, webSocketServer? }

--- package/environments/development.js
+++ package/environments/development.js
@@ -1,8 +1,10 @@
 const { merge } = require('webpack-merge')
 
 const baseConfig = require('./base')
-const devServerConfig = require('../dev_server')
+const devServer = require('../dev_server')
 const { runningWebpackDevServer } = require('../env')
+
+const { outputPath: contentBase, publicPath } = require('../config')
 
 let devConfig = {
   mode: 'development',
@@ -10,6 +12,34 @@ let devConfig = {
 }
 
 if (runningWebpackDevServer) {
+  const liveReload = devServer.live_reload !== undefined ? devServer.live_reload : !devServer.hmr
+
+  const devServerConfig = {
+    devMiddleware: {
+      publicPath
+    },
+    compress: devServer.compress,
+    allowedHosts: devServer.allowed_hosts,
+    host: devServer.host,
+    port: devServer.port,
+    https: devServer.https,
+    hot: devServer.hmr,
+    liveReload,
+    historyApiFallback: { disableDotRule: true },
+    headers: devServer.headers,
+    static: {
+      publicPath: contentBase
+    }
+  }
+
+  if (devServer.static) {
+    devServerConfig.static = { ...devServerConfig.static, ...devServer.static }
+  }
+
+  if (devServer.client) {
+    devServerConfig.client = devServer.client
+  }
+
   devConfig = merge(devConfig, {
     stats: {
       colors: true,
