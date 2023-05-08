eceaa765004b6a603dc1ca3fc9f6fbf850a5766a
Justin Gordon <justin@shakacode.com>
2023-05-07 19:36:25 -1000

    Handle all available devserver keys

--- package/dev_server.js
+++ package/dev_server.js
@@ -2,7 +2,7 @@
 const { isBoolean } = require('./utils/helpers')
 const config = require('./config')
 
-const fetch = (key) => {
+const envFetch = (key) => {
   const value = process.env[key]
   return isBoolean(value) ? JSON.parse(value) : value
 }
@@ -10,11 +10,13 @@ const fetch = (key) => {
 const devServerConfig = config.dev_server
 
 if (devServerConfig) {
-  const envPrefix = config.dev_server.env_prefix || 'WEBPACKER_DEV_SERVER'
+  const envPrefix = config.dev_server.env_prefix || 'SHAKAPACKER_DEV_SERVER'
 
   Object.keys(devServerConfig).forEach((key) => {
-    const envValue = fetch(`${envPrefix}_${key.toUpperCase()}`)
-    if (envValue !== undefined) devServerConfig[key] = envValue
+    const envValue = envFetch(`${envPrefix}_${key.toUpperCase()}`)
+    if (envValue !== undefined) {
+      devServerConfig[key] = envValue
+    }
   })
 }
 
--- package/environments/development.js
+++ package/environments/development.js
@@ -1,55 +1,13 @@
 const { merge } = require('webpack-merge')
 
 const baseConfig = require('./base')
-const devServer = require('../dev_server')
+const webpackDevServerConfig  = require('../webpackDevServerConfig')
 const { runningWebpackDevServer } = require('../env')
 
-const { outputPath: contentBase, publicPath } = require('../config')
-
-let devConfig = {
+const devConfig = {
   mode: 'development',
-  devtool: 'cheap-module-source-map'
-}
-
-if (runningWebpackDevServer) {
-  const liveReload = devServer.live_reload !== undefined ? devServer.live_reload : !devServer.hmr
-
-  const devServerConfig = {
-    devMiddleware: {
-      publicPath
-    },
-    compress: devServer.compress,
-    allowedHosts: devServer.allowed_hosts,
-    host: devServer.host,
-    port: devServer.port,
-    https: devServer.https,
-    hot: devServer.hmr,
-    liveReload,
-    historyApiFallback: { disableDotRule: true },
-    headers: devServer.headers,
-    static: {
-      publicPath: contentBase
-    }
-  }
-
-  if (devServer.static) {
-    devServerConfig.static = { ...devServerConfig.static, ...devServer.static }
-  }
-
-  if (devServer.client) {
-    devServerConfig.client = devServer.client
-  }
-
-  devConfig = merge(devConfig, {
-    stats: {
-      colors: true,
-      entrypoints: false,
-      errorDetails: true,
-      modules: false,
-      moduleTrace: false
-    },
-    devServer: devServerConfig
-  })
+  devtool: 'cheap-module-source-map',
+  ...(runningWebpackDevServer && { devServer: webpackDevServerConfig() })
 }
 
 module.exports = merge(baseConfig, devConfig)
