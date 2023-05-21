22d45cbacff0616f290e4a5bc8458338302c2f50
Mostafa Ahangarha <ahangarha@riseup.net>
2023-05-21 21:45:47 +0330

    Make the test, a very simple failing test

--- spec/generator_specs/generator_spec.rb
+++ spec/generator_specs/generator_spec.rb
@@ -15,109 +15,115 @@ describe "Generator" do
     # FileUtils.cp_r(BASE_RAILS_APP_PATH, TEMP_RAILS_APP_PATH)
 
     # Don't use --skip-git because we want .gitignore file to exist in the project
-    sh_in_dir(SPEC_PATH, %(
-      rails new temp-rails-app --skip-javascript --skip-bundle --skip-spring --skip-test --skip-active-record
-      rm -rf temp-rails-app/.git
-    ))
+    # sh_in_dir(SPEC_PATH, %(
+    #   rails new temp-rails-app --skip-javascript --skip-bundle --skip-spring --skip-test --skip-active-record
+    #   rm -rf temp-rails-app/.git
+    # ))
 
-    Bundler.with_unbundled_env do
-      sh_in_dir(TEMP_RAILS_APP_PATH, %(
-        bundle add shakapacker --path "#{GEM_ROOT}"
-        bundle install
-        FORCE=true bundle exec rails shakapacker:install
-      ))
-    end
+    # Bundler.with_unbundled_env do
+    #   sh_in_dir(TEMP_RAILS_APP_PATH, %(
+    #     bundle add shakapacker --path "#{GEM_ROOT}"
+    #     bundle install
+    #     #FORCE=true bundle exec rails shakapacker:install
+    #   ))
+    # end
+
+
   end
 
   after :all do
-    Dir.chdir(SPEC_PATH)
-    FileUtils.rm_rf(TEMP_RAILS_APP_PATH)
+    # Dir.chdir(SPEC_PATH)
+    # FileUtils.rm_rf(TEMP_RAILS_APP_PATH)
   end
 
-  it "creates `config/shakapacker.yml`" do
-    config_file_relative_path = "config/shakapacker.yml"
-    actual_content = read(path_in_the_app(config_file_relative_path))
-    expected_content = read(path_in_the_gem(config_file_relative_path))
-
-    expect(actual_content).to eq "expected_content"
+  it "fails" do
+    expect(false).to be_truthy
   end
 
-  it "replaces package.json with template file" do
-    actual_content = read(path_in_the_app("package.json"))
+  # it "creates `config/shakapacker.yml`" do
+  #   config_file_relative_path = "config/shakapacker.yml"
+  #   actual_content = read(path_in_the_app(config_file_relative_path))
+  #   expected_content = read(path_in_the_gem(config_file_relative_path))
 
-    expect(actual_content).to match /"name": "app",/
-  end
+  #   expect(actual_content).to eq "expected_content"
+  # end
 
-  it "creates webpack config directory and its files" do
-    expected_files = [
-      "webpack.config.js"
-    ]
+  # it "replaces package.json with template file" do
+  #   actual_content = read(path_in_the_app("package.json"))
 
-    Dir.chdir(path_in_the_app("config/webpack")) do
-      exisiting_files_in_config_webpack_dir = Dir.glob("*")
-      expect(exisiting_files_in_config_webpack_dir).to eq expected_files
-    end
-  end
+  #   expect(actual_content).to match /"name": "app",/
+  # end
 
-  it "adds binstubs" do
-    expected_binstubs = []
-    Dir.chdir(File.join(GEM_ROOT, "lib/install/bin")) do
-      expected_binstubs = Dir.glob("bin/*")
-    end
+  # it "creates webpack config directory and its files" do
+  #   expected_files = [
+  #     "webpack.config.js"
+  #   ]
 
-    Dir.chdir(File.join(TEMP_RAILS_APP_PATH, "bin")) do
-      actual_binstubs = Dir.glob("*")
-      expect(actual_binstubs).to include(*expected_binstubs)
-    end
-  end
+  #   Dir.chdir(path_in_the_app("config/webpack")) do
+  #     exisiting_files_in_config_webpack_dir = Dir.glob("*")
+  #     expect(exisiting_files_in_config_webpack_dir).to eq expected_files
+  #   end
+  # end
 
-  it "modifies .gitignore" do
-    actual_content = read(path_in_the_app(".gitignore"))
+  # it "adds binstubs" do
+  #   expected_binstubs = []
+  #   Dir.chdir(File.join(GEM_ROOT, "lib/install/bin")) do
+  #     expected_binstubs = Dir.glob("bin/*")
+  #   end
 
-    expect(actual_content).to match ".yarn-integrity"
-  end
+  #   Dir.chdir(File.join(TEMP_RAILS_APP_PATH, "bin")) do
+  #     actual_binstubs = Dir.glob("*")
+  #     expect(actual_binstubs).to include(*expected_binstubs)
+  #   end
+  # end
 
-  it 'adds <%= javascript_pack_tag "application" %>' do
-    actual_content = read(path_in_the_app("app/views/layouts/application.html.erb"))
+  # it "modifies .gitignore" do
+  #   actual_content = read(path_in_the_app(".gitignore"))
 
-    expect(actual_content).to match '<%= javascript_pack_tag "application" %>'
-  end
+  #   expect(actual_content).to match ".yarn-integrity"
+  # end
 
-  it "updates `bin/setup" do
-    setup_file_content = read(path_in_the_app("bin/setup"))
-    expect(setup_file_content).to match %r(^\s*system!\(['"]bin/yarn['"]\))
-  end
+  # it 'adds <%= javascript_pack_tag "application" %>' do
+  #   actual_content = read(path_in_the_app("app/views/layouts/application.html.erb"))
 
-  it "adds relevant shakapacker version in package.json depending on gem version," do
-    npm_version = Shakapacker::Utils::VersionSyntaxConverter.new.rubygem_to_npm(Shakapacker::VERSION)
+  #   expect(actual_content).to match '<%= javascript_pack_tag "application" %>'
+  # end
 
-    actual_content = read(path_in_the_app("package.json"))
+  # it "updates `bin/setup" do
+  #   setup_file_content = read(path_in_the_app("bin/setup"))
+  #   expect(setup_file_content).to match %r(^\s*system!\(['"]bin/yarn['"]\))
+  # end
 
-    expect(actual_content).to match /"shakapacker": "#{npm_version}",/
-  end
+  # it "adds relevant shakapacker version in package.json depending on gem version," do
+  #   npm_version = Shakapacker::Utils::VersionSyntaxConverter.new.rubygem_to_npm(Shakapacker::VERSION)
 
-  it "adds Shakapacker peer dependencies to package.json" do
-    package_json_content_in_app = read(path_in_the_app("package.json"))
+  #   actual_content = read(path_in_the_app("package.json"))
 
-    expected_dependencies = %w(
-      @babel/core
-      @babel/plugin-transform-runtime
-      @babel/preset-env
-      @babel/runtime
-      babel-loader
-      compression-webpack-plugin
-      terser-webpack-plugin
-      webpack
-      webpack-assets-manifest
-      webpack-cli
-      webpack-dev-server
-      webpack-merge
-    )
+  #   expect(actual_content).to match /"shakapacker": "#{npm_version}",/
+  # end
 
-    expected_dependencies.each do |package|
-      expect(package_json_content_in_app).to include package
-    end
-  end
+  # it "adds Shakapacker peer dependencies to package.json" do
+  #   package_json_content_in_app = read(path_in_the_app("package.json"))
+
+  #   expected_dependencies = %w(
+  #     @babel/core
+  #     @babel/plugin-transform-runtime
+  #     @babel/preset-env
+  #     @babel/runtime
+  #     babel-loader
+  #     compression-webpack-plugin
+  #     terser-webpack-plugin
+  #     webpack
+  #     webpack-assets-manifest
+  #     webpack-cli
+  #     webpack-dev-server
+  #     webpack-merge
+  #   )
+
+  #   expected_dependencies.each do |package|
+  #     expect(package_json_content_in_app).to include package
+  #   end
+  # end
 
   private
     def path_in_the_app(relative_path = nil)
