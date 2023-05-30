92dcba3b1c1d24b08c5cbab23ed6aba546a40483
Mostafa Ahangarha <ahangarha@riseup.net>
2023-05-30 14:33:16 +0330

    Add specs to check:binstubs

--- spec/shakapacker/rake_tasks_spec.rb
+++ spec/shakapacker/rake_tasks_spec.rb
@@ -1,10 +1,11 @@
+require "rake"
 require_relative "spec_helper_initializer"
 
 describe "RakeTasks" do
-  let(:test_app_path) { File.expand_path("../test_app", __dir__) }
+  TEST_APP_PATH = File.expand_path("../test_app", __dir__)
 
   it "`rake -T` lists Shakapacker tasks" do
-    output = Dir.chdir(test_app_path) { `rake -T` }
+    output = Dir.chdir(TEST_APP_PATH) { `rake -T` }
     expect(output).to include "shakapacker"
     expect(output).to include "shakapacker:check_binstubs"
     expect(output).to include "shakapacker:check_node"
@@ -17,18 +18,77 @@ describe "RakeTasks" do
   end
 
   it "`shakapacker:check_binstubs` doesn't get 'webpack binstub not found' error" do
-    output = Dir.chdir(test_app_path) { `rake shakapacker:check_binstubs 2>&1` }
+    output = Dir.chdir(TEST_APP_PATH) { `rake shakapacker:check_binstubs 2>&1` }
     expect(output).to_not include "webpack binstub not found."
   end
 
   it "`shakapacker:check_node` doesn't get 'shakapacker requires Node.js' error" do
-    output = Dir.chdir(test_app_path) { `rake shakapacker:check_node 2>&1` }
+    output = Dir.chdir(TEST_APP_PATH) { `rake shakapacker:check_node 2>&1` }
     expect(output).to_not include "Shakapacker requires Node.js"
   end
 
   it "`shakapacker:check_yarn` doesn't get error related to yarn" do
-    output = Dir.chdir(test_app_path) { `rake shakapacker:check_yarn 2>&1` }
+    output = Dir.chdir(TEST_APP_PATH) { `rake shakapacker:check_yarn 2>&1` }
     expect(output).to_not include "Yarn not installed"
     expect(output).to_not include "Shakapacker requires Yarn"
   end
+
+  describe "shakapacker:check_binstubs" do
+    before :all do
+      Dir.chdir(TEST_APP_PATH)
+    end
+
+    context "with existing `./bin/shapapacker` and `./bin/shapapacker-dev-server`" do
+      it "passes" do
+        expect { system("bundle exec rake shakapacker:check_binstubs") }.to output("").to_stdout_from_any_process
+      end
+    end
+
+    context "without `./bin/shakapacker`" do
+      before :all do
+        Rake.move("bin/shakapacker", "bin/shakapacker_renamed")
+      end
+
+      after :all do
+        Rake.move("bin/shakapacker_renamed", "bin/shakapacker")
+      end
+
+      it "passes if `./bin/webpacker exist" do
+        with_temporary_file("bin/webpacker") do
+          expect { system("bundle exec rake shakapacker:check_binstubs") }.to output(/DEPRECATION/).to_stdout_from_any_process
+        end
+      end
+
+      it "fails otherwise" do
+        expect { system("bundle exec rake shakapacker:check_binstubs") }.to output(/Couldn't find shakapacker binstubs!/).to_stdout_from_any_process
+      end
+    end
+
+    context "without `./bin/shakapacker-dev-server`" do
+      before :all do
+        Rake.move("bin/shakapacker-dev-server", "bin/shakapacker-dev-server_renamed")
+      end
+
+      after :all do
+        Rake.move("bin/shakapacker-dev-server_renamed", "bin/shakapacker-dev-server")
+      end
+
+      it "passes if `./bin/webpacker-dev-server exist" do
+        with_temporary_file("bin/webpacker-dev-server") do
+          expect { system("bundle exec rake shakapacker:check_binstubs") }.to output(/DEPRECATION/).to_stdout_from_any_process
+        end
+      end
+
+      it "fails otherwise" do
+        expect { system("bundle exec rake shakapacker:check_binstubs") }.to output(/Couldn't find shakapacker binstubs!/).to_stdout_from_any_process
+      end
+    end
+  end
+end
+
+def with_temporary_file(file_name, &block)
+  Rake.touch(file_name)
+  yield if block_given?
+ensure
+  Rake.rm_f(file_name)
 end
