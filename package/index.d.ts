26a46b27e61c02127fc1cd82b4418fa687ca88b7
Gareth Jones <jones258@gmail.com>
2023-07-07 09:35:57 +1200

    feat: add types

---
+++ package/index.d.ts
@@ -0,0 +1,22 @@
+declare module 'shakapacker' {
+  import { Configuration } from 'webpack';
+
+  export { merge } from 'webpack-merge';
+  export const globalMutableWebpackConfig: Configuration;
+  export function generateWebpackConfig(): Configuration;
+}
+
+declare module 'shakapacker/package/babel/preset.js' {
+  import { ConfigAPI, PluginItem, TransformOptions } from '@babel/core';
+
+  interface RequiredTransformOptions {
+    plugins: PluginItem[];
+    presets: PluginItem[];
+  }
+
+  const defaultConfigFunc: (
+    api: ConfigAPI
+  ) => TransformOptions & RequiredTransformOptions;
+
+  export = defaultConfigFunc;
+}
