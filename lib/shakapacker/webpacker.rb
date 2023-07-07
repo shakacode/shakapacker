b139a0430b51e58c39eb70a7b028fc7f865c39d2
Judah Meek <judah.meek@gmail.com>
2023-07-07 17:50:07 -0500

    consider constants as well

--- lib/shakapacker/webpacker.rb
+++ lib/shakapacker/webpacker.rb
@@ -5,4 +5,12 @@ class Webpacker
          "to their Shakpacker equivalent, such as Shakapacker.#{method_name}"
     Shakapacker.send(method_name, *args, &block)
   end
+
+  def self.const_missing(const_name)
+    puts "The Webpacker constant has been superseded by the Shakapacker constant. "\
+         "Please change all references to the Webpacker constant, such as Webpacker::#{const_name}, "\
+         "to their Shakpacker equivalent, such as Shakapacker::#{const_name}"
+    super unless const_name == :Manifest
+    Shakapacker::Manifest
+  end
 end
