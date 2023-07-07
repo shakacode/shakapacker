0b5b23cc446a991ab4edaedb64d551aa544836fd
Gareth Jones <jones258@gmail.com>
2023-07-08 08:26:39 +1200

    feat: type `env` constant properly

--- package/index.d.ts
+++ package/index.d.ts
@@ -1,12 +1,20 @@
 declare module 'shakapacker' {
   import { Configuration } from 'webpack'
 
+  interface Env {
+    railsEnv: string
+    nodeEnv: string
+    isProduction: boolean
+    isDevelopment: boolean
+    runningWebpackDevServer: boolean
+  }
+
   export const config: Record<string, unknown>
   export const devServer: Record<string, unknown>
   export function generateWebpackConfig(): Configuration
   export const globalMutableWebpackConfig: Configuration
   export const baseConfig: Record<string, unknown>
-  export const env: unknown
+  export const env: Env
   export const rules: Record<string, unknown>
   export function moduleExists(packageName: string): boolean
   export function canProcess<T = unknown>(rule: string, fn: (modulePath: string) => T): T | null
