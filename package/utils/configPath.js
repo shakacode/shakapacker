def32ab6939f1f9d95a0ae9793ad1e5cbcd9c466
Tom Dracz <tom.dracz@simplybusiness.co.uk>
2024-03-06 19:09:53 +0000

    Fix configPath

--- package/utils/configPath.js
+++ package/utils/configPath.js
@@ -1,10 +1,3 @@
-const fs = require('fs')
 const { resolve } = require('path')
 
-const resolveToPhysicalFilePath = () => {
-  const shakapackerConfigPath = resolve('config', 'shakapacker.yml')
-
-  if (fs.existsSync(shakapackerConfigPath)) return shakapackerConfigPath
-}
-
-module.exports = process.env.SHAKAPACKER_CONFIG || resolveToPhysicalFilePath()
+module.exports = process.env.SHAKAPACKER_CONFIG || resolve('config', 'shakapacker.yml')
