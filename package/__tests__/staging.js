8585bf85260a62a3dc2106f5499b0374fc6f6f70
Gareth Jones <Jones258@Gmail.com>
2024-03-09 22:34:04 +1300

    feat!: remove `globalMutableWebpackConfig` (#439)

--- package/__tests__/staging.js
+++ package/__tests__/staging.js
@@ -30,24 +30,4 @@ describe('Custom environment', () => {
       })
     })
   })
-
-  describe('globalMutableWebpackConfig', () => {
-    beforeEach(() => jest.resetModules())
-
-    test('should use staging config and default production environment', () => {
-      process.env.RAILS_ENV = 'staging'
-      delete process.env.NODE_ENV
-
-      const { globalMutableWebpackConfig: webpackConfig } = require('../index')
-
-      expect(webpackConfig.output.path).toEqual(
-        resolve('public', 'packs-staging')
-      )
-      expect(webpackConfig.output.publicPath).toEqual('/packs-staging/')
-      expect(webpackConfig).toMatchObject({
-        devtool: 'source-map',
-        stats: 'normal'
-      })
-    })
-  })
 })
