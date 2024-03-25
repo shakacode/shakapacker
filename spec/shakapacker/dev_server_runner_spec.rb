c99bf5b760c20f795537e97852c208d5a7501bb0
Gareth Jones <jones258@gmail.com>
2024-03-26 07:28:00 +1300

    feat: check the package manager is obvious before running commands

--- spec/shakapacker/dev_server_runner_spec.rb
+++ spec/shakapacker/dev_server_runner_spec.rb
@@ -1,7 +1,20 @@
 require_relative "spec_helper_initializer"
 require "shakapacker/dev_server_runner"
 
+def within_temp_directory(tmpdir = nil, &block)
+  Dir.mktmpdir("shakapacker-", tmpdir) do |dir|
+    Dir.chdir(dir, &block)
+  end
+end
+
 describe "DevServerRunner" do
+  around do |example|
+    within_temp_directory do
+      FileUtils.cp_r(File.expand_path("./test_app", __dir__), Dir.pwd)
+      Dir.chdir("test_app") { example.run }
+    end
+  end
+
   before do
     @original_node_env, ENV["NODE_ENV"] = ENV["NODE_ENV"], "development"
     @original_rails_env, ENV["RAILS_ENV"] = ENV["RAILS_ENV"], "development"
@@ -14,12 +27,22 @@ describe "DevServerRunner" do
     ENV["SHAKAPACKER_CONFIG"] = @original_shakapacker_config
   end
 
-  let(:test_app_path) { File.expand_path("./test_app", __dir__) }
+  let(:test_app_path) { Dir.pwd }
 
   NODE_PACKAGE_MANAGERS.each do |fallback_manager|
     context "when using package_json with #{fallback_manager} as the manager" do
-      with_package_json_fallback_manager(fallback_manager)
+      before do
+        manager_name = fallback_manager.split("_")[0]
+        manager_version = "1.2.3"
+        manager_version = "4.5.6" if fallback_manager == "yarn_berry"
 
+        PackageJson.read.merge! { { "packageManager" => "#{manager_name}@#{manager_version}" } }
+
+        allow(Shakapacker::ManagerChecker).to receive(:new).and_return(fake_manager_checker)
+        allow(fake_manager_checker).to receive(:warn_unless_package_manager_is_obvious!)
+      end
+
+      let(:fake_manager_checker) { instance_double(Shakapacker::ManagerChecker).as_null_object }
       let(:package_json) { PackageJson.read(test_app_path) }
 
       require "package_json"
@@ -117,6 +140,7 @@ describe "DevServerRunner" do
         klass.run(argv)
 
         expect(Kernel).to have_received(:exec).with(env, *cmd)
+        expect(fake_manager_checker).to have_received(:warn_unless_package_manager_is_obvious!)
       end
     end
 end
