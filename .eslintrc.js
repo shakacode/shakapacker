ef16c492017421561c174764c38ec666728d149c
Gareth Jones <jones258@gmail.com>
2024-04-06 08:17:40 +1300

    chore: enable a few more jest rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,21 @@ module.exports = {
       files: ["test/**"],
       extends: ["plugin:jest/recommended", "plugin:jest/style"],
       rules: {
-        "global-require": "off"
+        "global-require": "off",
+        "jest/prefer-called-with": "error",
+        "jest/no-conditional-in-test": "error",
+        "jest/no-test-return-statement": "error",
+        "jest/prefer-expect-resolves": "error",
+        "jest/require-to-throw-message": "error",
+        "jest/require-top-level-describe": "error",
+        "jest/prefer-hooks-on-top": "error",
+        "jest/prefer-lowercase-title": [
+          "error",
+          { ignoreTopLevelDescribe: true }
+        ],
+        "jest/prefer-spy-on": "error",
+        // "jest/prefer-strict-equal": "error",
+        "jest/prefer-todo": "error"
       }
     }
   ]
