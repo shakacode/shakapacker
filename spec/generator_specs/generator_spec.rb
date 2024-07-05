0f066f2b60fd11784659968d51944c8c1e77aa87
adriangohjw <adriangohjw@gmail.com>
2024-07-05 16:25:10 +0800

    manually overwrite "rack" to be v2

--- spec/generator_specs/generator_spec.rb
+++ spec/generator_specs/generator_spec.rb
@@ -21,9 +21,13 @@ describe "Generator" do
     Bundler.with_unbundled_env do
       if RUBY_VERSION.start_with?("2.")
         # Bundler's version compatible with Ruby 2 does not support "--path" switch
+        # Overwriting "rack" version due to unless Rack::Handler::Puma.respond_to?(:config) in Capybara gem v3.39.2 or earlier.
+        # Issue resolved in Capybara v3.40.0, but Ruby 2.7 support dropped; last compatible version is v3.39.2.
+        # Ref: https://github.com/shakacode/shakapacker/issues/498
         sh_in_dir({}, BASE_RAILS_APP_PATH, %(
           gem update bundler
           echo 'gem "shakapacker", :path => "#{GEM_ROOT}"' >> Gemfile
+          echo 'gem "rack", "< 3.0.0"' >> Gemfile
           bundle install
         ))
       else
