7591e953367048b2e879b9924d5bc96598989f64
Alexey Romanov <alexey.romanov+ci5@shakacode.com>
2025-03-13 12:11:46 +0300

    Simplify rules (#557)
    Co-authored-by: Alexey Romanov <alexey.v.romanov@gmail.com>

--- package/rules/index.js
+++ package/rules/index.js
@@ -1,20 +1,16 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
 
-const rules = {
-  raw: require("./raw"),
-  file: require("./file"),
-  css: require("./css"),
-  sass: require("./sass"),
-  babel: require("./babel"),
-  swc: require("./swc"),
-  esbuild: require("./esbuild"),
-  erb: require("./erb"),
-  coffee: require("./coffee"),
-  less: require("./less"),
-  stylus: require("./stylus")
-}
-
-module.exports = Object.keys(rules)
-  .filter((key) => !!rules[key])
-  .map((key) => rules[key])
+module.exports = [
+  require("./raw"),
+  require("./file"),
+  require("./css"),
+  require("./sass"),
+  require("./babel"),
+  require("./swc"),
+  require("./esbuild"),
+  require("./erb"),
+  require("./coffee"),
+  require("./less"),
+  require("./stylus")
+].filter(Boolean)
