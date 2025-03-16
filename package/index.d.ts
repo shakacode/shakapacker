e2cc49b0071d049197be68f270669e92a69c25c3
Alexey Romanov <alexey.v.romanov@gmail.com>
2025-03-16 08:06:32 +0000

    Duplicate webpack-dev-server types instead of importing them

--- package/index.d.ts
+++ package/index.d.ts
@@ -1,6 +1,6 @@
 declare module 'shakapacker' {
   import { Configuration, RuleSetRule } from 'webpack'
-  import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
+  import * as https from 'node:https';
 
   export interface Config {
     source_path: string
@@ -33,18 +33,40 @@ declare module 'shakapacker' {
     runningWebpackDevServer: boolean
   }
 
-  type CamelToSnakeCase<S extends string> = S extends `${infer First}${infer Rest}`
-    ? Rest extends Uncapitalize<Rest> // Check if `Rest` starts with a lowercase letter
-      ? `${Lowercase<First>}${CamelToSnakeCase<Rest>}`
-      : `${Lowercase<First>}_${CamelToSnakeCase<Uncapitalize<Rest>>}`
-    : S
+  type Header = Array<{ key: string; value: string }> | Record<string, string | string[]>
+  type ServerType = 'http' | 'https' | 'spdy'
+  type WebSocketType = 'sockjs' | 'ws'
 
-  type SnakeCase<T> = {
-    [K in keyof T as CamelToSnakeCase<string & K>]: T[K];
-  }
-
-  type DevServerConfig = SnakeCase<Omit<WebpackDevServerConfiguration, 'hot'>> & {
-    hmr?: boolean
+  /**
+   * This has the same keys and behavior as https://webpack.js.org/configuration/dev-server/ except:
+   * 1. `hot` is replaced by `hmr`;
+   * 2. Camel-cased properties are replaced by snake-cased ones.
+   * @see {import('webpack-dev-server').Configuration}
+   */
+  interface DevServerConfig {
+    allowed_hosts?: 'all' | 'auto' | string | string[]
+    bonjour?: boolean | Record<string, unknown> // bonjour.BonjourOptions
+    client?: Record<string, unknown> // Client
+    compress?: boolean
+    dev_middleware?: Record<string, unknown> // webpackDevMiddleware.Options
+    headers?: Header | (() => Header)
+    history_api_fallback?: boolean | Record<string, unknown> // HistoryApiFallbackOptions
+    hmr?: 'only' | boolean
+    host?: 'local-ip' | 'local-ipv4' | 'local-ipv6' | string
+    http2?: boolean
+    https?: boolean | https.ServerOptions
+    ipc?: boolean | string
+    magic_html?: boolean
+    live_reload?: boolean
+    open?: boolean | string | string[] | Record<string, unknown> | Record<string, unknown>[]
+    port?: 'auto' | string | number
+    proxy?: unknown // ProxyConfigMap | ProxyConfigArray
+    setup_exit_signals?: boolean
+    static?: boolean | string | unknown // Static | Array<string | Static>
+    watch_files?: string | string[] | unknown // WatchFiles | Array<WatchFiles | string>
+    web_socket_server?: string | boolean | WebSocketType | { type?: string | boolean | WebSocketType, options?: Record<string, unknown> }
+    server?: string | boolean | ServerType | { type?: string | boolean | ServerType, options?: https.ServerOptions }
+    [otherWebpackDevServerConfigKey: string]: unknown
   }
 
   export const config: Config
