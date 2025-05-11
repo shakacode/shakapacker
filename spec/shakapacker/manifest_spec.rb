9c32386da346641631085556bf08727d34fe2083
Panagiotis Plytas <plytas.panagiotis16@gmail.com>
2025-05-11 22:34:25 +0300

    Add tests for subresource integrity (#506)

--- spec/shakapacker/manifest_spec.rb
+++ spec/shakapacker/manifest_spec.rb
@@ -3,98 +3,206 @@ require_relative "spec_helper_initializer"
 describe "Shakapacker::Manifest" do
   let(:manifest_path) { File.expand_path File.join(File.dirname(__FILE__), "./test_app/public/packs", "manifest.json").to_s }
 
-  it "#lookup! raises an exception for a non-existing asset file" do
-    asset_file = "calendar.js"
-    expected_error_message = "Shakapacker can't find #{asset_file} in #{manifest_path}"
+  context "with no integrity hashes" do
+    it "#lookup! raises an exception for a non-existing asset file" do
+      asset_file = "calendar.js"
+      expected_error_message = "Shakapacker can't find #{asset_file} in #{manifest_path}"
 
-    allow(Shakapacker.config).to receive(:compile?).and_return(false)
+      allow(Shakapacker.config).to receive(:compile?).and_return(false)
 
-    expect {
-      Shakapacker.manifest.lookup!(asset_file)
-    }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+      expect {
+        Shakapacker.manifest.lookup!(asset_file)
+      }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+    end
+
+    it "#lookup! raises an exception for a non-existing asset file with type and without an extension" do
+      asset_file = "calendar"
+      expected_error_message = "Shakapacker can't find #{asset_file}.js in #{manifest_path}"
+
+      allow(Shakapacker.config).to receive(:compile?).and_return(false)
+
+      expect {
+        Shakapacker.manifest.lookup!(asset_file, type: :javascript)
+      }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+    end
+
+    it "#lookup! returns the path to the bundled bootstrap.js" do
+      actual = Shakapacker.manifest.lookup!("bootstrap.js")
+      expected = "/packs/bootstrap-300631c4f0e0f9c865bc.js"
+
+      expect(actual).to eq expected
+    end
+
+    it "#lookup_pack_with_chunks! returns an array of paths to the bundled bootstrap of type javascript" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("bootstrap", type: :javascript)
+      expected = ["/packs/bootstrap-300631c4f0e0f9c865bc.js"]
+
+      expect(actual).to eq expected
+    end
+
+    it "#lookup_with_chunks! returns an array of paths to the bundled bootstrap.js of type javascript" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("bootstrap.js", type: :javascript)
+      expected = ["/packs/bootstrap-300631c4f0e0f9c865bc.js"]
+
+      expect(actual).to eq expected
+    end
+
+    it "#lookup_with_chunks! returns an array of paths to the bundled 'print/application' without an extension and in a sub-directory" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("print/application", type: :css)
+      expected = ["/packs/print/application-983b6c164a47f7ed49cd.css"]
+
+      expect(actual).to eq expected
+    end
+
+    it "#lookup_with_chunks! returns an array of paths to the bundled 'print/application.css' in a sub-directory" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("print/application.css", type: :css)
+      expected = ["/packs/print/application-983b6c164a47f7ed49cd.css"]
+
+      expect(actual).to eq expected
+    end
+
+    it "#lookup returns nil for non-existing asset files" do
+      expect(Shakapacker.manifest.lookup("foo.js")).to be nil
+    end
+
+    it "#lookup_pack_with_chunks returns nil for non-existing asset files" do
+      expect(Shakapacker.manifest.lookup_pack_with_chunks("foo.js")).to be nil
+    end
+
+    it "#lookup returns the path for bootstrap.js" do
+      actual = Shakapacker.manifest.lookup("bootstrap.js")
+      expected = "/packs/bootstrap-300631c4f0e0f9c865bc.js"
+
+      expect(actual).to eq expected
+    end
+
+    it "#lookup_pack_with_chunks! raises an exception for non-existing asset files" do
+      asset_file = "calendar"
+
+      expected_error_message = "Shakapacker can't find #{asset_file}.js in #{manifest_path}"
+
+      allow(Shakapacker.config).to receive(:compile?).and_return(false)
+
+      expect {
+        Shakapacker.manifest.lookup_pack_with_chunks!(asset_file, type: :javascript)
+      }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+    end
+
+    it "#lookup_pack_with_chunks! returns an array of paths to bundled js files with 'application' in their name" do
+      actual_application_entrypoints = Shakapacker.manifest.lookup_pack_with_chunks!("application", type: :javascript)
+      expected_application_entrypoints = [
+        "/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js",
+        "/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js",
+        "/packs/application-k344a6d59eef8632c9d1.js"
+      ]
+
+      expect(actual_application_entrypoints).to eq expected_application_entrypoints
+    end
   end
 
-  it "#lookup! raises an exception for a non-existing asset file with type and without an extension" do
-    asset_file = "calendar"
-    expected_error_message = "Shakapacker can't find #{asset_file}.js in #{manifest_path}"
+  context "with integrity hashes" do
+    it "#lookup! raises an exception for a non-existing asset file" do
+      asset_file = "calendar_with_integrity.js"
+      expected_error_message = "Shakapacker can't find #{asset_file} in #{manifest_path}"
 
-    allow(Shakapacker.config).to receive(:compile?).and_return(false)
+      allow(Shakapacker.config).to receive(:compile?).and_return(false)
 
-    expect {
-      Shakapacker.manifest.lookup!(asset_file, type: :javascript)
-    }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
-  end
+      expect {
+        Shakapacker.manifest.lookup!(asset_file)
+      }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+    end
 
-  it "#lookup! returns the path to the bundled bootstrap.js" do
-    actual = Shakapacker.manifest.lookup!("bootstrap.js")
-    expected = "/packs/bootstrap-300631c4f0e0f9c865bc.js"
+    it "#lookup! raises an exception for a non-existing asset file with type and without an extension" do
+      asset_file = "calendar_with_integrity"
+      expected_error_message = "Shakapacker can't find #{asset_file}.js in #{manifest_path}"
 
-    expect(actual).to eq expected
-  end
+      allow(Shakapacker.config).to receive(:compile?).and_return(false)
 
-  it "#lookup_pack_with_chunks! returns an array of paths to the bundled bootstrap of type javascript" do
-    actual = Shakapacker.manifest.lookup_pack_with_chunks!("bootstrap", type: :javascript)
-    expected = ["/packs/bootstrap-300631c4f0e0f9c865bc.js"]
+      expect {
+        Shakapacker.manifest.lookup!(asset_file, type: :javascript)
+      }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+    end
 
-    expect(actual).to eq expected
-  end
+    it "#lookup! returns the path to the bundled bootstrap_with_integrity.js" do
+      actual = Shakapacker.manifest.lookup!("bootstrap_with_integrity.js")
+      expected = "/packs/bootstrap_with_integrity-300631c4f0e0f9c865bc.js"
 
-  it "#lookup_with_chunks! returns an array of paths to the bundled bootstrap.js of type javascript" do
-    actual = Shakapacker.manifest.lookup_pack_with_chunks!("bootstrap.js", type: :javascript)
-    expected = ["/packs/bootstrap-300631c4f0e0f9c865bc.js"]
+      expect(actual).to eq expected
+    end
 
-    expect(actual).to eq expected
-  end
+    it "#lookup_pack_with_chunks! returns an array of paths to the bundled bootstrap_with_integrity of type javascript" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("bootstrap_with_integrity", type: :javascript)
+      expected = [{ "src" => "/packs/bootstrap-300631c4f0e0f9c865bc.js", "integrity" => "sha384-hash" }]
 
-  it "#lookup_with_chunks! returns an array of paths to the bundled 'print/application' without an extension and in a sub-directory" do
-    actual = Shakapacker.manifest.lookup_pack_with_chunks!("print/application", type: :css)
-    expected = ["/packs/print/application-983b6c164a47f7ed49cd.css"]
+      expect(actual).to eq expected
+    end
 
-    expect(actual).to eq expected
-  end
+    it "#lookup_with_chunks! returns an array of paths to the bundled bootstrap_with_integrity.js of type javascript" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("bootstrap_with_integrity.js", type: :javascript)
+      expected = [{ "src" => "/packs/bootstrap-300631c4f0e0f9c865bc.js", "integrity" => "sha384-hash" }]
 
-  it "#lookup_with_chunks! returns an array of paths to the bundled 'print/application.css' in a sub-directory" do
-    actual = Shakapacker.manifest.lookup_pack_with_chunks!("print/application.css", type: :css)
-    expected = ["/packs/print/application-983b6c164a47f7ed49cd.css"]
+      expect(actual).to eq expected
+    end
 
-    expect(actual).to eq expected
-  end
+    it "#lookup_with_chunks! returns an array of paths to the bundled 'print/application_with_integrity' without an extension and in a sub-directory" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("print/application_with_integrity", type: :css)
+      expected = [{ "src" => "/packs/print/application-983b6c164a47f7ed49cd.css", "integrity" => "sha384-hash" }]
 
-  it "#lookup returns nil for non-existing asset files" do
-    expect(Shakapacker.manifest.lookup("foo.js")).to be nil
-  end
+      expect(actual).to eq expected
+    end
 
-  it "#lookup_pack_with_chunks returns nil for non-existing asset files" do
-    expect(Shakapacker.manifest.lookup_pack_with_chunks("foo.js")).to be nil
-  end
+    it "#lookup_with_chunks! returns an array of paths to the bundled 'print/application_with_integrity.css' in a sub-directory" do
+      actual = Shakapacker.manifest.lookup_pack_with_chunks!("print/application_with_integrity.css", type: :css)
+      expected = [{ "src" => "/packs/print/application-983b6c164a47f7ed49cd.css", "integrity" => "sha384-hash" }]
 
-  it "#lookup returns the path for bootstrap.js" do
-    actual = Shakapacker.manifest.lookup("bootstrap.js")
-    expected = "/packs/bootstrap-300631c4f0e0f9c865bc.js"
+      expect(actual).to eq expected
+    end
 
-    expect(actual).to eq expected
-  end
+    it "#lookup returns nil for non-existing asset files" do
+      expect(Shakapacker.manifest.lookup("foo_with_integrity.js")).to be nil
+    end
 
-  it "#lookup_pack_with_chunks! raises an exception for non-existing asset files" do
-    asset_file = "calendar"
+    it "#lookup_pack_with_chunks returns nil for non-existing asset files" do
+      expect(Shakapacker.manifest.lookup_pack_with_chunks("foo_with_integrity.js")).to be nil
+    end
 
-    expected_error_message = "Shakapacker can't find #{asset_file}.js in #{manifest_path}"
+    it "#lookup returns the path for bootstrap_with_integrity.js" do
+      actual = Shakapacker.manifest.lookup("bootstrap_with_integrity.js")
+      expected = "/packs/bootstrap_with_integrity-300631c4f0e0f9c865bc.js"
 
-    allow(Shakapacker.config).to receive(:compile?).and_return(false)
+      expect(actual).to eq expected
+    end
 
-    expect {
-      Shakapacker.manifest.lookup_pack_with_chunks!(asset_file, type: :javascript)
-    }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
-  end
+    it "#lookup_pack_with_chunks! raises an exception for non-existing asset files" do
+      asset_file = "calendar_with_integrity"
 
-  it "#lookup_pack_with_chunks! returns an array of paths to bundled js files with 'application' in their name" do
-    actual_application_entrypoints = Shakapacker.manifest.lookup_pack_with_chunks!("application", type: :javascript)
-    expected_application_entrypoints = [
-      "/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js",
-      "/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js",
-      "/packs/application-k344a6d59eef8632c9d1.js"
-    ]
+      expected_error_message = "Shakapacker can't find #{asset_file}.js in #{manifest_path}"
 
-    expect(actual_application_entrypoints).to eq expected_application_entrypoints
+      allow(Shakapacker.config).to receive(:compile?).and_return(false)
+
+      expect {
+        Shakapacker.manifest.lookup_pack_with_chunks!(asset_file, type: :javascript)
+      }.to raise_error(Shakapacker::Manifest::MissingEntryError, /#{expected_error_message}/)
+    end
+
+    it "#lookup_pack_with_chunks! returns an array of paths to bundled js files with 'application_with_integrity' in their name" do
+      actual_application_entrypoints = Shakapacker.manifest.lookup_pack_with_chunks!("application_with_integrity", type: :javascript)
+      expected_application_entrypoints = [
+        {
+          "src" => "/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js",
+          "integrity" => "sha384-hash",
+        },
+        {
+          "src" => "/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js",
+          "integrity" => "sha384-hash"
+        },
+        {
+          "src" => "/packs/application-k344a6d59eef8632c9d1.js",
+          "integrity" => "sha384-hash"
+        }
+      ]
+
+      expect(actual_application_entrypoints).to eq expected_application_entrypoints
+    end
   end
 end
