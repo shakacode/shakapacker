5422036b42a7c52a0060d30d774646d5baeaa2eb
Craig McNamara <craig@monami.io>
2025-08-19 00:52:17 -0700

    Fix static asset handling

--- package/rspack/rules/file.js
+++ package/rspack/rules/file.js
@@ -1,8 +1,29 @@
-// Use Rspack's built-in asset modules instead of file-loader
+const { dirname } = require("path")
+const {
+  additional_paths: additionalPaths,
+  source_path: sourcePath
+} = require("../config")
+
 module.exports = {
-  test: /\.(bmp|gif|jpe?g|png|tiff|ico|avif|webp|svg|eot|otf|ttf|woff|woff2)$/,
+  test: /\.(bmp|gif|jpe?g|png|tiff|ico|avif|webp|eot|otf|ttf|woff|woff2|svg)$/,
+  exclude: /\.(js|mjs|jsx|ts|tsx)$/,
   type: "asset/resource",
   generator: {
-    filename: "media/[name]-[hash][ext]"
+    filename: (pathData) => {
+      const path = dirname(pathData.filename)
+      const stripPaths = [...additionalPaths, sourcePath]
+
+      const selectedStripPath = stripPaths.find((includePath) =>
+        path.startsWith(includePath)
+      )
+
+      const folders = path
+        .replace(`${selectedStripPath}`, "")
+        .split("/")
+        .filter(Boolean)
+
+      const foldersWithStatic = ["static", ...folders].join("/")
+      return `${foldersWithStatic}/[name]-[hash][ext][query]`
+    }
   }
 }
