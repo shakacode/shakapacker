e761a153f24cb47a59659daf2fcbc6e5667ced29
Craig McNamara <craig@monami.io>
2025-08-19 00:35:37 -0700

    Fix asset loaders

--- package/rspack/rules/css.js
+++ package/rspack/rules/css.js
@@ -1,19 +1,19 @@
 const { rspack } = require("@rspack/core")
-const { getStyleRule } = require("../../utils/getStyleRule")
-const { isProduction } = require("../../env")
 const { inliningCss } = require("../../utils/inliningCss")
+const { moduleExists } = require("../../utils/helpers")
 
-// Use Rspack's built-in CSS handling
-const cssLoader = {
-  loader: "css-loader",
-  options: {
-    sourceMap: true,
-    importLoaders: 1,
-    modules: false
-  }
-}
-
-module.exports = getStyleRule(/\.(css)$/i, [
-  inliningCss ? "style-loader" : rspack.CssExtractRspackPlugin.loader,
-  cssLoader
-])
+// Simple CSS rule for rspack - don't use getStyleRule to avoid complexity
+module.exports = moduleExists("css-loader") ? {
+  test: /\.(css)$/i,
+  use: [
+    inliningCss ? "style-loader" : rspack.CssExtractRspackPlugin.loader,
+    {
+      loader: "css-loader",
+      options: {
+        sourceMap: true,
+        importLoaders: 1,
+        modules: false
+      }
+    }
+  ]
+} : null
