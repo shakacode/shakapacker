f22b0af88edb3dd08a9bd24cb0d24eb03d93e8f9
Justin Gordon <justin@shakacode.com>
2025-09-25 15:42:00 -1000

    Fix ESLint formatting issues
    - Fix prettier formatting in package/config.js
    - Fix object shorthand in package/rspack/index.js

--- package/rspack/index.js
+++ package/rspack/index.js
@@ -31,7 +31,7 @@ const generateRspackConfig = (extraConfig = {}, ...extraArgs) => {
   const rspackConfig = {
     ...environmentConfig,
     module: {
-      rules: rules
+      rules
     },
     plugins: getPlugins(),
     optimization: getOptimization()
