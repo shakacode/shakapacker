9290faefe2e05dadc881b4098de8cb7a8b94dae6
Justin Gordon <justin@shakacode.com>
2025-09-28 12:24:00 -1000

    fix: Add missing utility TypeScript files
    - Added debug.ts for logging utilities
    - Added getStyleRule.ts for CSS loader configuration
    - These utilities are needed by the rules and optimization files
    - Were accidentally removed during TypeScript refactoring

---
+++ package/utils/getStyleRule.ts
@@ -0,0 +1,59 @@
+/* eslint global-require: 0 */
+const { canProcess, moduleExists } = require("./helpers")
+const { requireOrError } = require("./requireOrError")
+const config = require("../config")
+const inliningCss = require("./inliningCss")
+
+interface StyleRule {
+  test: RegExp
+  use: any[]
+  type?: string
+}
+
+const getStyleRule = (test: RegExp, preprocessors: any[] = []): StyleRule | null => {
+  if (moduleExists("css-loader")) {
+    const tryPostcss = () =>
+      canProcess("postcss-loader", (loaderPath: string) => ({
+        loader: loaderPath,
+        options: { sourceMap: true }
+      }))
+
+    // style-loader is required when using css modules with HMR on the webpack-dev-server
+
+    const extractionPlugin =
+      config.assets_bundler === "rspack"
+        ? requireOrError("@rspack/core").CssExtractRspackPlugin.loader
+        : requireOrError("mini-css-extract-plugin").loader
+
+    const use = [
+      inliningCss ? "style-loader" : extractionPlugin,
+      {
+        loader: require.resolve("css-loader"),
+        options: {
+          sourceMap: true,
+          importLoaders: 2,
+          modules: {
+            auto: true
+          }
+        }
+      },
+      tryPostcss(),
+      ...preprocessors
+    ].filter(Boolean)
+
+    const result: StyleRule = {
+      test,
+      use
+    }
+
+    if (config.assets_bundler === "rspack") {
+      result.type = "javascript/auto"
+    }
+
+    return result
+  }
+
+  return null
+}
+
+export = { getStyleRule }
