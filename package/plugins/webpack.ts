89b189a828aa9c178f5f78c2bcf347ffde40e8e9
Judah Meek <judah.meek@gmail.com>
2025-11-02 17:40:01 -0600

    remove unused import

--- package/plugins/webpack.ts
+++ package/plugins/webpack.ts
@@ -5,7 +5,7 @@ const { requireOrError } = require("../utils/requireOrError")
 const WebpackAssetsManifest = requireOrError("webpack-assets-manifest")
 const webpack = requireOrError("webpack")
 const config = require("../config") as Config
-const { isProduction, isDevelopment } = require("../env")
+const { isProduction } = require("../env")
 const { moduleExists } = require("../utils/helpers")
 
 const getPlugins = (): unknown[] => {
