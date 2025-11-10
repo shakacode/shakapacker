787d9f2dd1226d79eb89b8b7157ea2905524227a
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-11-11 10:25:43 +1300

    fix: use `@ts-ignore` instead of `@ts-expect-error` in public types (#828)
    ### Summary
    
    `@ts-expect-error` will error if there isn't an error, so should only be
    used in cases where that will always be the case - for the public types,
    this import is only expected to be an error if `webpack` types are not
    available so we need to use `@ts-ignore` instead otherwise downstream
    typechecking of libs will fail

--- package/loaders.d.ts
+++ package/loaders.d.ts
@@ -1,4 +1,4 @@
-// @ts-expect-error: webpack is an optional peer dependency (using type-only import)
+// @ts-ignore: webpack is an optional peer dependency (using type-only import)
 import type { LoaderDefinitionFunction } from "webpack"
 
 export interface ShakapackerLoaderOptions {
