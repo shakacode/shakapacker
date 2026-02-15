3dec21b85beef476172d5e6f1af2e18ff8322db5
Justin Gordon <justin@shakacode.com>
2026-02-14 22:39:41 -1000

    Switch default file rule to webpack asset module

--- package/rules/file.ts
+++ package/rules/file.ts
@@ -8,7 +8,7 @@ const {
 export = {
   test: /\.(bmp|gif|jpe?g|png|tiff|ico|avif|webp|eot|otf|ttf|woff|woff2|svg)$/,
   exclude: /\.(js|mjs|jsx|ts|tsx)$/,
-  type: "asset/resource",
+  type: "asset",
   generator: {
     filename: (pathData: { filename?: string }) => {
       // Guard against null/undefined pathData or filename
