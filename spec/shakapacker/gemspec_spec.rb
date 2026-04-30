09ddc1b1642d363885ae159bf3092c03a783d912
Justin Gordon <justin@shakacode.com>
2026-04-29 23:11:49 -1000

    Reduce packaged gem files

--- spec/shakapacker/gemspec_spec.rb
+++ spec/shakapacker/gemspec_spec.rb
@@ -22,14 +22,38 @@ describe "shakapacker.gemspec" do
       expect(node_modules_files).to be_empty
     end
 
+    it "excludes JavaScript package source" do
+      package_files = gemspec.files.select { |f| f.start_with?("package/") }
+      expect(package_files).to be_empty
+    end
+
+    it "excludes repository-only documentation and test files" do
+      excluded_files = gemspec.files.select { |f| f.start_with?("docs/", "test/") }
+      expect(excluded_files).to be_empty
+    end
+
     it "includes lib directory" do
       lib_files = gemspec.files.select { |f| f.start_with?("lib/") }
       expect(lib_files).not_to be_empty
     end
 
+    it "includes install assets needed by shakapacker:install" do
+      expect(gemspec.files).to include(
+        "lib/install/template.rb",
+        "lib/install/package.json",
+        "lib/install/config/shakapacker.yml",
+        "lib/install/bin/shakapacker",
+        "lib/install/bin/shakapacker-dev-server"
+      )
+    end
+
     it "includes RBS type signatures" do
       rbs_files = gemspec.files.select { |f| f.end_with?(".rbs") }
       expect(rbs_files).not_to be_empty
+    end
+
+    it "does not package test files through test_files metadata" do
+      expect(gemspec.test_files).to be_empty
     end
   end
 end
